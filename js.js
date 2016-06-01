151e75c0c2a48732ded59dfcd83d7100465a9e86
monarch086 <sbarsuk88@gmail.com>
2016-06-01 15:33:49 +0300

    w

--- js.js
+++ js.js
@@ -3,32 +3,85 @@
 var rC = 0; //random card
 var userScore = 0;
 var computerScore = 0;
+var riskIndex = 18; //до каких очков комп продолжает игру
+var stopGame = false;
 
 function addCard() {
-    rC = Math.floor(Math.random() * 13) + 2;
-    userScore += rC;
-    document.getElementById('text_one').firstChild.nodeValue = "Your score: " + userScore;
-    document.getElementById('left_card').src = "img/Cards/" + rC + "_pic.png";
-    
-    setTimeout(computerStep, 1000);
-    
-    if (userScore == 21) {         document.getElementById('text_one').firstChild.nodeValue = "You won: BLACKJACK";
+    if (stopGame == false) {
+		rC = Math.floor(Math.random() * 13) + 2;
+		userScore += rC;
+		document.getElementById('text_one').firstChild.nodeValue = "Your score: " + userScore;
+		document.getElementById('left_card').src = "img/Cards/" + rC + "_pic.png";
     }
-    else if (userScore > 21) {
-    document.getElementById('text_one').firstChild.nodeValue = "You lost";
-    }
-    
+	
+	setTimeout(computerStep, 1000);
+	setTimeout(verification, 1010);
 }
 
 function computerStep() {
-    rC = Math.floor(Math.random() * 13) + 2;
-    computerScore += rC;
-    document.getElementById('text_two').firstChild.nodeValue = "Your opponent`s score: " + computerScore;
-    document.getElementById('right_card').src = "img/Cards/" + rC + "_pic.png";
+    if (computerScore < riskIndex) {
+		rC = Math.floor(Math.random() * 13) + 2;
+		computerScore += rC;
+		document.getElementById('text_two').firstChild.nodeValue = "Your opponent`s score: " + computerScore;
+		document.getElementById('right_card').src = "img/Cards/" + rC + "_pic.png";
+	}
+}
+
+function verification() {
+	var obj = document.getElementById('btnStr');
+		
+	if (userScore == 21 && computerScore == 21) {
+		document.getElementById('text_win').firstChild.nodeValue = "Standoff!";
+		endGame();
+	}
+	else if (userScore > 21 && computerScore > 21) {
+		document.getElementById('text_win').firstChild.nodeValue = "Standoff!";
+		endGame();
+	}
+	else if (userScore == 21 && computerScore < 21) {
+		document.getElementById('text_win').firstChild.nodeValue = "BLACKJACK!";
+		endGame();
+    }
+	else if (userScore > 21 && computerScore <= 21) {
+		document.getElementById('text_win').firstChild.nodeValue = "You lost!";
+		endGame();
+    }
+	else if (userScore < 21 && computerScore > 21) {
+		document.getElementById('text_win').firstChild.nodeValue = "You won!";
+		endGame();
+	}
+	else if (userScore < 21 && computerScore == 21) {
+		document.getElementById('text_win').firstChild.nodeValue = "You lost!";
+		endGame();
+	}
+	else if (stopGame == true && computerScore >= riskIndex) {
+		if (userScore > computerScore) {
+			document.getElementById('text_win').firstChild.nodeValue = "You won!";
+			endGame();
+		}
+		else if (userScore < computerScore) {
+			document.getElementById('text_win').firstChild.nodeValue = "You lost!";
+			endGame();
+		}
+		else if (userScore == computerScore) {
+			document.getElementById('text_win').firstChild.nodeValue = "Standoff!";
+			endGame();
+		}
+		stopGame = false;
+	}
+}
+
+function endGame() {
+	stopGame = false;
+	document.getElementById('text_win').style.display = "block";
+	document.getElementById('btnAdd').disabled = true;
+	document.getElementById('btnStop').disabled = true;
+	document.getElementById('btnStr').style.display = "block";
 }
 
 function stop() {
-    
+    stopGame = true;
+	addCard();
 }
 
 function start() {
@@ -40,6 +93,12 @@ function start() {
     
     document.getElementById('text_two').firstChild.nodeValue = "Your opponent`s score: " + computerScore;
     document.getElementById('right_card').src = "img/Cards/2_pic.png";
+	
+	document.getElementById('btnStr').style.display = "none";
+	document.getElementById('text_win').style.display = "none";
+	
+	document.getElementById('btnAdd').disabled = false;
+	document.getElementById('btnStop').disabled = false;
 }
 
 document.getElementById('btnAdd').onclick = addCard;
