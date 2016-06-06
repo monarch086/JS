0a83b59f51a0f427a9289493dbc66f1b620d0401
monarch086 <sbarsuk88@gmail.com>
2016-06-06 22:28:36 +0300

    home

--- Laminate/js.js
+++ Laminate/js.js
@@ -1,33 +1,83 @@
 ﻿'use strict';
 var lRoom, wRoom, lLam, wLam, minLen, offset, laying,
 	canvas = document.getElementById("canv"),
-	context = canvas.getContext('2d');
-
+	context = canvas.getContext('2d'),
+	numberOfRows, //кол-во рядов ламината
+	remainder = 0, //размер последнего оставшегося куска
+	sumOfRemainder = 0, //кол-во оставшихся кусков
+	arrOfRemainder, //массив остатков
+	correctionOfMin = 0; //смещение ряда для избежания мелких остатков
 
 function draw(){
-	lRoom = document.getElementById("lRoom").value;
-	wRoom = document.getElementById("wRoom").value;
-	lLam = document.getElementById("lLam").value;
-	wLam = document.getElementById("wLam").value;
-	
+	initializeVars();
 	clear();
+	
 	canvas.width = wRoom;
 	canvas.height = lRoom;
 	context.strokeRect(0, 0, wRoom, lRoom);
 	
 	context.beginPath();
-	var currentPosition = wLam;
-	while (currentPosition <= wRoom) {
-		context.moveTo(0, currentPosition);
-		context.lineTo(wRoom, currentPosition);
+	var currentYPosition = wLam;
+	var currentXPosition = lLam;
+	
+	for(var i = 0; i < numberOfRows; i++) {
+		context.moveTo(0, currentYPosition);
+		context.lineTo(wRoom, currentYPosition);
 		context.stroke();
-		currentPosition += wLam;
-		context.moveTo(0, currentPosition);
+		currentYPosition += wLam;
 	}
 	
+	currentYPosition = 0;
+	
+	for(var i = 0; i < numberOfRows - 1; i++) {
+		//alert("i = " + i);
+		setCorrectionOfMin();
+		currentXPosition -= correctionOfMin;
+		while(currentXPosition < wRoom) {
+			context.moveTo(currentXPosition, currentYPosition);
+			context.lineTo(currentXPosition, (currentYPosition + wLam));
+			context.stroke();
+			context.moveTo(currentXPosition, currentYPosition);
+			currentXPosition += lLam;
+		}
+		correctionOfMin = 0;
+		remainder = currentXPosition - wRoom;
+		if(remainder < minLen) {
+			remainder = 0;
+			sumOfRemainder++;
+			currentXPosition = lLam;
+		}
+		else {
+			currentXPosition = remainder;
+		}
+		currentYPosition += wLam;
+	}
 }
 
-function clear(){
+function setCorrectionOfMin() {
+	if(remainder > 0) {
+		correctionOfMin = 0; //to do
+	}
+	else {
+		if((wRoom%lLam) < minLen) {
+			correctionOfMin = minLen - (wRoom%lLam);
+		}
+		if((lLam - correctionOfMin) < minLen) {
+			correctionOfMin = lLam - Math.floor(((lLam - correctionOfMin) + minLen) / 2);
+		}
+	}
+}
+
+function initializeVars() {
+	lRoom = document.getElementById("lRoom").value;
+	wRoom = document.getElementById("wRoom").value;
+	lLam = document.getElementById("lLam").value / 10;
+	wLam = document.getElementById("wLam").value / 10;
+	numberOfRows = Math.ceil(wRoom / wLam);
+	minLen = document.getElementById("minLen").value / 10;
+}
+
+function clear() {
 	var hcanv = canvas.height,
 		wcanv = canvas.width;
 	context.clearRect(0, 0, wcanv, hcanv);
