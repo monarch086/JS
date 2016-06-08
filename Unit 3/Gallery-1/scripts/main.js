4f50639d97de5fee1f58b9fe77158fd4206e244c
monarch086 <sbarsuk88@gmail.com>
2016-06-08 17:37:35 +0300

    w eve

--- Unit 3/Gallery-1/scripts/main.js
+++ Unit 3/Gallery-1/scripts/main.js
@@ -2,9 +2,9 @@ var menu, box1, box2,
 	kittens = [],
 	puppies = [],
 	fishes = [],
-	currentPosition = 0, //смещение слайдера относительно начала
-	currentAnimalClass; //текущий класс картинок (животных)
-
+	array,	//указатель на текущий масив 
+	currentPosition = 0; //смещение слайдера относительно начала
+	
 window.onload = start;
 function start() {
 	menu = document.getElementById("menu");
@@ -27,36 +27,45 @@ function slot1(event) { //отработка нажатий в меню
 	console.log(item);
 	console.log(category);
 	if (category === "Котята") {
-		currentAnimalClass = 'kittens';
+		array = kittens;
 		currentPosition = 0;
-		loadSlider();
+		
 	} else if (category === "Щеночки") {
-		currentAnimalClass = 'puppies';
+		array = puppies;
 		currentPosition = 0;
-		loadSlider();
+		
 	} else {
-		currentAnimalClass = 'fishes';
+		array = fishes;
 		currentPosition = 0;
-		loadSlider();
+		
 	}
+	clearBox2();
+	loadSlider();
 }
 
 function slot2(event) { //отработка нажатий на элементы слайдера
 	var x = event.srcElement;
 	console.log(x);
+	
 	var p = x.src;
-	if(x == document.getElementById("left"))
+	if(x == document.getElementById("left")) 		
 		moveLeft();
 	
-	else if(x == document.getElementById("right"))
+	else if(x == document.getElementById("right")) 
 		moveRight();
-	
+			
 	else{
 		box2.innerHTML = '<img src="' + p + '">';
 		restorePics(x.className);
 		x.style.width = '220px';
 		x.style.height = '170px';
 	}
+}
+
+function clearBox2() { //очистить Box2 (поле с большой фотографией)
+	var elems = box2.getElementsByTagName('img');
+	if(elems.length > 0)
+		box2.removeChild(elems[0]);
 }
 
 function restorePics(className) { //восстановить размер всех элементов слайдера до минимального
@@ -75,7 +84,7 @@ function moveLeft() {
 }
 
 function moveRight() {
-	if(currentPosition < fishes.length - 4) {
+	if(currentPosition < array.length - 4) {
 		currentPosition++;
 		loadSlider();
 	}
@@ -83,14 +92,8 @@ function moveRight() {
 
 function loadSlider() { //загрузка картинок слайдера
 	var code = '<img src="images/navigate-left.png" id="left"/>';
-	for (var i = currentPosition; i < currentPosition + 4; i++) {
-		if(currentAnimalClass == 'kittens')
-			code += kittens[i];
-		else if(currentAnimalClass == 'puppies')
-			code += puppies[i];
-		else if(currentAnimalClass == 'fishes')
-			code += fishes[i];
-	}
+	for (var i = currentPosition; i < currentPosition + 4; i++) 
+		code += array[i];
 	code += '<img src="images/navigate-right.png" id="right"/>';
 	box1.innerHTML = code;
 }
